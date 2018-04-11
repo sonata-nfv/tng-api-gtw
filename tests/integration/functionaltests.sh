3645bff58e74b5fcb5b2e1d8b56e2282747285c5
José Bonnet <jose.bonnet@gmail.com>
2018-04-11 16:33:12 +0100

    Example package and functional test added

---
+++ tests/integration/functionaltests.sh
@@ -0,0 +1,57 @@
+## Copyright (c) 2015 SONATA-NFV, 2017 5GTANGO [, ANY ADDITIONAL AFFILIATION]
+## ALL RIGHTS RESERVED.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+## Neither the name of the SONATA-NFV, 5GTANGO [, ANY ADDITIONAL AFFILIATION]
+## nor the names of its contributors may be used to endorse or promote
+## products derived from this software without specific prior written
+## permission.
+##
+## This work has been performed in the framework of the SONATA project,
+## funded by the European Commission under Grant number 671517 through
+## the Horizon 2020 and 5G-PPP programmes. The authors would like to
+## acknowledge the contributions of their colleagues of the SONATA
+## partner consortium (www.sonata-nfv.eu).
+##
+## This work has been performed in the framework of the 5GTANGO project,
+## funded by the European Commission under Grant number 761493 through
+## the Horizon 2020 and 5G-PPP programmes. The authors would like to
+## acknowledge the contributions of their colleagues of the 5GTANGO
+## partner consortium (www.5gtango.eu).
+# encoding: utf-8
+#
+# This file holds the integration tests
+## Variables
+FIXTURES_FOLDER="./fixtures"
+TEST_PACKAGE_FILE="5gtango-ns-package-example.tgo"
+PRE_INTEGRATION="https://pre-int-sp-ath.5gtango.eu:32001"
+
+# Test package file presence
+echo "Testing package file presence..."
+if  ! [ -e "$FIXTURES_FOLDER/$TEST_PACKAGE_FILE" ]
+then
+    echo "Test package file $TEST_PACKAGE_FILE not found in $FIXTURES_FOLDER folder"
+fi
+echo "    ...done!"
+
+# Testing package file upload
+echo "Testing package file upload..."
+#curl -X POST $PRE_INTEGRATION -F x=1 -F y=2 -F package=@"$FIXTURES_FOLDER/$TEST_PACKAGE_FILE"
+curl -X POST $PRE_INTEGRATION package=@"$FIXTURES_FOLDER/$TEST_PACKAGE_FILE"
+if [ $? -ne 0 ]
+then
+    echo "Package file $FIXTURES_FOLDER/$TEST_PACKAGE_FILE upload to $PRE_INTEGRATION failled with $?"
+    exit 1
+fi
+echo "    ...done!"
