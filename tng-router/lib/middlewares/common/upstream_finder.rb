913cf7c87b76eada75334fea20eef3d2d112a9bb
José Bonnet <jose.bonnet@gmail.com>
2018-05-09 18:16:33 +0100

    Added unit tests and fixes for calculating routing

--- tng-router/lib/middlewares/common/upstream_finder.rb
+++ tng-router/lib/middlewares/common/upstream_finder.rb
@@ -45,46 +45,84 @@ class UpstreamFinder
   def initialize(app, options={})
     @app, @paths = app, options[:paths]
     @base_path = options[:base_path] || ''
+    @logger = Logger.new(STDERR)
     $stdout.puts "Initialized #{self.class.name} with base_path=#{@base_path} and paths=#{@paths}"
   end
 
   def call(env)
     msg = self.class.name+'#'+__method__.to_s
-    env['5gtango.logger'] = Logger.new(STDERR) if env['5gtango.logger'].to_s.empty?
-    
-    @logger = env['5gtango.logger']
-    env['5gtango.logger'].info(msg) {"Called"}
-    request = Rack::Request.new(env)
-    env['5gtango.logger'].debug(msg) {"Base_path=#{@base_path} and paths=#{@paths}"}
-    
-    env['5gtango.logger'].debug(msg) {"REQUEST_PATH: #{env["REQUEST_PATH"]}"}
-    simple_path = env["REQUEST_PATH"]
-    simple_path.slice!(@base_path)
-    path = find_path(env["REQUEST_PATH"])
-    return bad_request("Error finding #{request.request_method}") if path.nil?
-    path[:verbs] = [ 'get' ] unless path.key?(:verbs)
-    
-    not_found("#{request.request_method} is not supported by #{path[:site]}, only #{path[:verbs].join(', ')}") unless method_ok?(path[:verbs], request.request_method)
-    #forbidden("#{request.request_method}ing into #{path[:site]} needs authentication") unless authenticated?(path, env)
-    env['5gtango.sink_path'] = path[:site]+query_string?(env["QUERY_STRING"])
-    env['5gtango.logger'].debug(msg) {"path built: #{env['5gtango.sink_path']}"}
+    env['5gtango.logger'] = @logger if env['5gtango.logger'].to_s.empty?
+    @logger.info(msg) {"Called"}
+    @logger.debug(msg) {"Env=#{env}"}
+    env['5gtango.sink_path'] = build_path(Rack::Request.new(env))
+    @logger.debug(msg) {"path built: #{env['5gtango.sink_path']}"}
     status, headers, body = @app.call(env)
-    env['5gtango.logger'].debug(msg) {"Finishing with status #{status}"}
+    @logger.debug(msg) {"Finishing with status #{status}"}
     respond(status, headers, body)
   end
   
+  def build_path(request)
+    msg = self.class.name+'#'+__method__.to_s
+    @logger.debug(msg) {"Base_path='#{@base_path}' and paths=#{@paths}"}
+    @logger.debug(msg) {"request.path: #{request.path}"}
+    @logger.debug(msg) {"request.path_info: #{request.path_info}"}
+    @logger.debug(msg) {"request.script_name: #{request.script_name}"}
+    @logger.debug(msg) {"request.query_string: #{request.query_string}"}
+    @logger.debug(msg) {"request.host_with_port: #{request.host_with_port}"}
+    @logger.debug(msg) {"request.referer: #{request.referer}"}
+    @logger.debug(msg) {"request.url: #{request.url}"}
+    
+    simple_path = request.path
+    simple_path.slice!(@base_path) unless @base_path == ''
+    router_path = find_router_path(request.path)
+    return bad_request("Error finding #{request.request_method}") if router_path.nil?
+    @logger.debug(msg) {"router_path: #{router_path}"}
+    @paths[router_path][:verbs] = [ 'get' ] unless @paths[router_path].key?(:verbs)
+    
+    not_found("#{request.request_method} is not supported by #{@paths[router_path][:site]}, only #{@paths[router_path][:verbs].join(', ')}") unless method_ok?(@paths[router_path][:verbs], request.request_method)
+    #forbidden("#{request.request_method}ing into #{path[:site]} needs authentication") unless authenticated?(path, env)
+    
+    # "/api/v3/packages(/?|/*)" =>  ["/api/v3/packages/", "/api/v3/packages", "/api/v3/packages/{+splat}"]
+    path_templates=Mustermann.new(router_path.to_s).to_templates
+    # p1=Mustermann.new("/api/v3/packages/{+splat}")
+    # p1.match("/api/v3/packages/status/123")[:splat] => "status/123"
+    @logger.debug(msg) {"path_templates: #{path_templates}"}
+    final_path = ''
+    path_templates.each do |template|
+      @logger.debug(msg) {"template: #{template.inspect}"}
+      # match_data, area_code = *"555-867-5309".match(regex)
+      full_match, *match = *Mustermann.new(template).match(simple_path)
+      @logger.debug(msg) {"full_match: #{full_match.inspect}"}
+      @logger.debug(msg) {"match: #{match.inspect}"}
+      #if match.is_a?(MatchData)
+      #  final_path = match.fetch(:splat, '')
+      #  @logger.debug(msg) {"final_path: #{final_path}"}
+      #  break
+      #end
+      if full_match
+        final_path = match.first
+        @logger.debug(msg) {"final_path: #{final_path}"}
+        break
+      end
+    end
+    # WRONG! We need the rest of the string
+    # .../api/v3/packages/status/1234... is not being correctely translated into .../packages/status/1234...
+    @paths[router_path][:site]+final_path?(final_path)+query_string?(request.query_string)
+  end
+  
   private
-  def find_path(request_path)
+  def find_router_path(request_path)
     msg = self.class.name+'#'+__method__.to_s
     chomped_path = request_path
-    chomped_path.slice!(@base_path)
+    chomped_path.slice!(@base_path) unless @base_path == ''
     @logger.debug(msg) {"chomped_path: #{chomped_path}"}
     
     possible_paths = @paths.keys.select { |path| chomped_path =~ Mustermann.new(path.to_s)}
     @logger.debug(msg) {"possible_paths: #{possible_paths}"}
-    longest_path = possible_paths.max_by(&:length)
-    return nil if longest_path.nil?
-    @paths[longest_path.to_sym]
+    #longest_path = possible_paths.max_by(&:length)
+    #return nil if longest_path.nil?
+    #@paths[longest_path.to_sym]
+    possible_paths.first
   end
   
   def method_ok?(allowed_methods, method)
@@ -113,6 +151,10 @@ class UpstreamFinder
     a.keys
   end
   
+  def final_path?(str)
+    str.to_s.empty? ? '' : '/'+str
+  end
+
   def query_string?(str)
     str.empty? ? '' : '?'+str
   end
