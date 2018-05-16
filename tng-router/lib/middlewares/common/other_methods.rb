7b5632e416a9a9d5746bde8d608016375c94c65f
José Bonnet <jose.bonnet@gmail.com>
2018-05-16 16:45:32 +0100

    Adds headers required for OPTIONS

--- tng-router/lib/middlewares/common/other_methods.rb
+++ tng-router/lib/middlewares/common/other_methods.rb
@@ -37,6 +37,8 @@ require 'rack/uploads'
 require 'curb'
 require 'faraday'
 require 'tempfile'
+require 'net/http'
+require 'uri'
 require_relative '../../utils'
 
 class OtherMethods
@@ -46,35 +48,53 @@ class OtherMethods
   
   def initialize(app, options= {})
     @app = app
-    puts "Initialized #{self.class.name}"
+    STDERR.puts "Initialized #{self.class.name}"
   end
 
   def call(env)
     msg = self.class.name+'#'+__method__.to_s
     env['5gtango.logger'].info(msg) {"Called"}
-    url = env['5gtango.sink_path'.freeze]
+    #url = env['5gtango.sink_path'.freeze]
     request = Rack::Request.new(env)  
     
     # Pass non-HEAD, DELETE or OPTIONS requests
     return @app.call(env) unless (request.head? || request.options? || request.delete?)
-    connection = Faraday.new(url) { |conn| conn.adapter :net_http }
+    
+    #connection = Faraday.new(url) { |conn| conn.adapter :net_http }
+    # from https://augustl.com/blog/2010/ruby_net_http_cheat_sheet/
+    uri = URI.parse(env['5gtango.sink_path'.freeze])
+    http = Net::HTTP.new(uri.host, uri.port)
+    
     method_name = request.request_method.downcase.to_sym
     # from https://stackoverflow.com/questions/35667746/preflight-options-request-with-faraday-gem
-    if method_name == :options
-      resp = connection.run_request(:options, nil, nil, nil) do |req|
-        req.url url
-        req.headers['Content-Type'] = request.content_type
-      end
-    elsif connection.respond_to?(method_name) && [:delete, :head].include?(method_name)
-      resp = connection.public_send(method_name) do |req|
-        req.url url
-        req.headers['Content-Type'] = request.content_type
-      end
+    case method_name
+    when :options
+      #resp = connection.run_request(:options, nil, nil, nil) do |req|
+      #  req.url url
+      #  req.headers['Content-Type'] = request.content_type
+      #end
+      http_request = Net::HTTP::Options.new(uri.request_uri)
+      http_request["Content-Type"] = request.content_type
+      response = http.request(http_request)
+      response.header['Access-Control-Allow-Origin'] = '*'
+      response.header['Access-Control-Allow-Methods'] = 'POST,GET,DELETE'      
+      response.header['Access-Control-Allow-Headers'] = 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
+    when :head
+      http_request = Net::HTTP::Head.new(uri.request_uri)
+      response = http.request(http_request)
+    when :delete
+      http_request = Net::HTTP::Delete.new(uri.request_uri)
+      response = http.request(http_request)
+      #elsif connection.respond_to?(method_name) && [:delete, :head].include?(method_name)
+      #resp = connection.public_send(method_name) do |req|
+      #  req.url url
+      #  req.headers['Content-Type'] = request.content_type
+      #end
     else
       return bad_request("HTTP method (#{request.request_method} not supported")
     end
-    env['5gtango.logger'].debug(msg) {"Response was #{resp}"}
-    respond(resp.status, resp.headers, resp.body)
+    env['5gtango.logger'].debug(msg) {"Response was #{response}"}
+    respond(response.status, response.headers, response.body)
   end    
   
   private
@@ -83,3 +103,29 @@ class OtherMethods
     (content_type =~ /application\/json/) || (content_type =~ /application\/yaml/) || (content_type =~ /application\/xml/)
   end
 end
+
+=begin
+http = Net::HTTP.new(uri.host, uri.port)
+http.open_timeout = 3 # in seconds
+http.read_timeout = 3 # in seconds
+
+# The request.
+request = Net::HTTP::Get.new(uri.request_uri)
+
+# All the HTTP 1.1 methods are available.
+Net::HTTP::Get
+Net::HTTP::Post
+Net::HTTP::Put
+Net::HTTP::Delete
+Net::HTTP::Head
+Net::HTTP::Options
+
+request.body = "Request body here."
+request.initialize_http_header({"Accept" => "*/*"})
+request.basic_auth("username", "password")
+
+response = http.request(request)
+response.body
+response.status
+response["header-here"] # All headers are lowercase
+=end
