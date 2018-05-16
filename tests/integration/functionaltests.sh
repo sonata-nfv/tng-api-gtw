c34c69e0648ae2785b5e0d869f2c8ea0055bcd5c
José Bonnet <jose.bonnet@gmail.com>
2018-05-16 14:30:27 +0100

    Closes #55

--- tests/integration/functionaltests.sh
+++ tests/integration/functionaltests.sh
@@ -41,8 +41,6 @@ PACKAGES_PRE_INTEGRATION_URL="$PRE_INTEGRATION_URL/packages"
 # Test package file presence
 echo "Testing package file presence..."
 echo "PWD is $(pwd)"
-FILES_PRESENT=$(ls)
-echo "Files present: $FILES_PRESENT"
 
 if  ! [ -e "$FIXTURES_FOLDER/$TEST_PACKAGE_FILE" ]
 then
@@ -53,90 +51,72 @@ echo "    ...done!"
 
 # Testing package file upload
 echo "Testing package file upload..."
-#curl -X POST $PRE_INTEGRATION -F x=1 -F y=2 -F package=@"$FIXTURES_FOLDER/$TEST_PACKAGE_FILE"
-#REGISTER_RESPONSE=$(curl -qSfsw '\n%{http_code}' -d '{"username":"'$USER'","password":"'$PASSWORD'","user_type":"developer","email":"'"$USER"'@sonata-nfv.eu"}' sp.int.sonata-nfv.eu:32001/api/v2/users)
-#echo "REGISTER_RESPONSE was $REGISTER_RESPONSE"
-#RESP=$(curl -qSfs -d '{"username":"sonata-'$NONCE'","password":"1234"}' http://sp.int.sonata-nfv.eu:32001/api/v2/sessions)
-#echo "User $USER logged in: $RESP"
-#token=$(echo $RESP | jq -r '.token.access_token')
 
 UPLOAD_RESPONSE=$(curl -qfsS -X POST $PACKAGES_PRE_INTEGRATION_URL -F package=@"$FIXTURES_FOLDER/$TEST_PACKAGE_FILE")
-echo "UPLOAD_RESPONSE=$UPLOAD_RESPONSE"
-PROCESS_UUID=$(echo $UPLOAD_RESPONSE | jq -r '.package_process_uuid')
-echo "PROCESS_UUID=$PROCESS_UUID"
-if [ -z "$PROCESS_UUID" ]; then
+echo "    UPLOAD_RESPONSE=$UPLOAD_RESPONSE"
+PACKAGE_PROCESS_UUID=$(echo $UPLOAD_RESPONSE | jq -r '.package_process_uuid')
+echo "    PACKAGE_PROCESS_UUID=$PACKAGE_PROCESS_UUID"
+if [ -z "$PACKAGE_PROCESS_UUID" ]; then
   echo "Package file $FIXTURES_FOLDER/$TEST_PACKAGE_FILE upload to $PACKAGES_PRE_INTEGRATION_URL failled with $UPLOAD_RESPONSE"
   exit 1
 fi
 echo "    ...successfuly!"
-echo "Getting package status..."
-PACKAGE_PROCESS_STATUS=$(curl -qfsS "$PACKAGES_PRE_INTEGRATION_URL/status/$PROCESS_UUID")
-
-#!/bin/bash
-
-# This script copies files from my homedirectory into the webserver directory.
-# (use scp and SSH keys for a remote directory)
-# A new directory is created every hour.
-
-#PICSDIR=/home/carol/pics
-#WEBDIR=/var/www/carol/webcam
-
-#while true; do 
-#	DATE=`date +%Y%m%d`
-#	HOUR=`date +%H`
-#	mkdir $WEBDIR/"$DATE"
-	
-#	while [ $HOUR -ne "00" ]; do 
-#		DESTDIR=$WEBDIR/"$DATE"/"$HOUR"
-#		mkdir "$DESTDIR"
-#		mv $PICDIR/*.jpg "$DESTDIR"/
-#		sleep 3600
-#		HOUR=`date +%H`
-#	done
-  #done
-echo "PACKAGE_PROCESS_STATUS=$PACKAGE_PROCESS_STATUS"
+TIMES_TO_RUN=5
+while [ $TIMES_TO_RUN -ne 0 ]
+do
+  ((TIMES_TO_RUN--))
+  echo "Getting package status..."
+  PACKAGE_PROCESS_DATA=$(curl -qfsS "$PACKAGES_PRE_INTEGRATION_URL/status/$PACKAGE_PROCESS_UUID")
+  echo "    PACKAGE_PROCESS_DATA=$PACKAGE_PROCESS_DATA"
+  PACKAGE_PROCESS_STATUS=$(echo $PACKAGE_PROCESS_DATA | jq -r '.package_process_status')
+  echo "    PACKAGE_PROCESS_STATUS=$PACKAGE_PROCESS_STATUS"
+  if [ "$PACKAGE_PROCESS_STATUS" == "running" ]; then
+    echo "Package file $FIXTURES_FOLDER/$TEST_PACKAGE_FILE processing failled with $PACKAGE_PROCESS_DATA"
+    sleep 5
+    continue
+  fi
+  if [ "$PACKAGE_PROCESS_STATUS" == "failed" ]; then
+    ERROR=$(echo $PACKAGE_PROCESS_DATA | jq -r '.package_metadata.error')
+    echo "Package file $FIXTURES_FOLDER/$TEST_PACKAGE_FILE processing failled with error '$ERROR'"
+    exit 1
+  fi
+  if [ $PACKAGE_PROCESS_STATUS == "success" ]; then 
+    break
+  fi
+done
+echo "    PACKAGE_PROCESS_STATUS=$PACKAGE_PROCESS_STATUS"
+if [ "$PACKAGE_PROCESS_STATUS" != "success" ]; then
+  echo "Package file $FIXTURES_FOLDER/$TEST_PACKAGE_FILE processing failled with $PACKAGE_PROCESS_DATA"
+  exit 1
+fi
 echo "Getting package uuid..."
-echo "    ...not done yet!"
+PACKAGE_UUID=$(echo $PACKAGE_PROCESS_DATA | jq -r '.package_id')
+echo "    PACKAGE_UUID=$PACKAGE_UUID"
+if [ -z "$PACKAGE_UUID" ]; then
+  echo "Package file $FIXTURES_FOLDER/$TEST_PACKAGE_FILE upload failled with no package UUID"
+  exit 1
+fi
 echo "Getting package meta-data..."
-echo "    ...not done yet!"
+PACKAGE_META_DATA_CODE=$(curl --write-out %{http_code} --silent --output /dev/null "$PACKAGES_PRE_INTEGRATION_URL/$PACKAGE_UUID")
+echo "    PACKAGE_META_DATA_CODE=$PACKAGE_META_DATA_CODE"
+if [ "$PACKAGE_META_DATA_CODE" != "200" ]; then
+  echo "Package file $FIXTURES_FOLDER/$TEST_PACKAGE_FILE meta-data download failled with code $PACKAGE_META_DATA_CODE"
+  exit 1
+fi
 echo "Getting package file..."
 echo "    ...not done yet!"
 echo "Deleting the package..."
-echo "    ...not done yet!"
+PACKAGE_META_DATA_CODE=$(curl -X DELETE --write-out %{http_code} --silent --output /dev/null "$PACKAGES_PRE_INTEGRATION_URL/$PACKAGE_UUID")
+echo "\tPACKAGE_META_DATA_CODE=$PACKAGE_META_DATA_CODE"
+if [ "$PACKAGE_META_DATA_CODE" != "204" ]; then
+  echo "Package file $FIXTURES_FOLDER/$TEST_PACKAGE_FILE meta-data delete failled with code $PACKAGE_META_DATA_CODE"
+  exit 1
+fi
 echo "Verify that package has been deleted..."
-echo "    ...not done yet!"
+PACKAGE_META_DATA_CODE=$(curl --write-out %{http_code} --silent --output /dev/null "$PACKAGES_PRE_INTEGRATION_URL/$PACKAGE_UUID")
+echo "    PACKAGE_META_DATA_CODE=$PACKAGE_META_DATA_CODE"
+if [ "$PACKAGE_META_DATA_CODE" != "404" ]; then
+  echo "Package file $FIXTURES_FOLDER/$TEST_PACKAGE_FILE meta-data delete failled with code $PACKAGE_META_DATA_CODE"
+  exit 1
+fi
 echo "    ...done!"
-
-# Using tavern
-# tavern-ci package-uuid.yaml --stdout --debug
-#get token
-#test_name: get_package
-
-#stages:
-#  - name: get_token
-#    request:
-#      url: http://sp.int3.sonata-nfv.eu:32001/api/v2/sessions
-#      json: 
-#        username: sonata
-#        password: '1234'
-#      method: POST
-#      headers:
-#        Content-Type: application/json
-#    response: 
-#      status_code: 200
-#      headers:
-#        content-type: application/json
-#      save:
-#        body:
-#          test_login_token: token.access_token
-
-#get 1 package
-#  - name: get_package
-#    request:
-#      url: http://sp.int3.sonata-nfv.eu:32001/api/v2/packages/d02b95b3-ef02-4b46-8548-af07eb789baa
-#      method: GET
-#      headers:
-#        content-type: application/json
-#        authorization: "Bearer {test_login_token:s}"
-#    response:
-#      status_code: 200
