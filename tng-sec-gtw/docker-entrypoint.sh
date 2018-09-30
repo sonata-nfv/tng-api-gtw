a2434c128902bb4e5a68c8e85f67ee51bd4d2fd3
Felipe Vicens <felipe.vicens@atos.net>
2018-09-30 13:42:26 +0200

    Adapting tng-sec-gtw to for kubernetes

--- tng-sec-gtw/docker-entrypoint.sh
+++ tng-sec-gtw/docker-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+sed -e "s/{DOMAIN}/$DOMAIN/g" /default-no-ssl-template.conf > /default-no-ssl.conf
+sed -e "s/{DOMAIN}/$DOMAIN/g" /default-ssl-template.conf > /default-ssl.conf
+
 if [ -f /etc/nginx/cert/sonata.crt ] && [ -f /etc/nginx/cert/sonata.key ]
 then
    echo "Starting 5GTANGO (V&V or Service) Platform" > /dev/stdout
-   rm -f /etc/nginx/conf.d/default-no-ssl.conf
+   ln -s /default-ssl.conf /etc/nginx/conf.d/default-ssl.conf
+   ln -s -f /default.conf /etc/nginx/conf.d/default.conf
 else
    echo "NO CERTIFICATES AVAILABLE" > /dev/stdout
    echo "/etc/nginx/cert/sonata.crt AND /etc/nginx/cert/sonata.key Should exists" > /dev/stdout
    echo "Running 5GTANGO (V&V or Service) Platform without HTTPS" > /dev/stdout
-   rm -f /etc/nginx/conf.d/default-ssl.conf
-   mv /etc/nginx/conf.d/default-no-ssl.conf /etc/nginx/conf.d/default.conf
+   ln -s -f /default-no-ssl.conf /etc/nginx/conf.d/default.conf
 fi
 
 exec $(which nginx) -c /etc/nginx/nginx.conf -g "daemon off;"
