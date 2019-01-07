99f4e50f6d0ad26e61c403fd1ec5878c7c1c3bd0
José Bonnet <jose.bonnet@icloud.com>
2019-01-07 14:00:48 +0000

    Fix wrong (outdated) references to logger

--- tng-router/lib/utils.rb
+++ tng-router/lib/utils.rb
@@ -54,4 +54,14 @@ module Utils
   def choose_logger(env)
     (env['rack.logger'.freeze]) ? env['rack.logger'.freeze] : Rack::NullLogger
   end
+  def symbolize(obj)
+    return obj.reduce({}) do |memo, (k, v)|
+      memo.tap { |m| m[k.to_sym] = symbolize(v) }
+    end if obj.is_a? Hash
+
+    return obj.reduce([]) do |memo, v| 
+      memo << symbolize(v); memo
+    end if obj.is_a? Array
+    obj
+  end
 end
